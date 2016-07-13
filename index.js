08bf2ab46b9972baa79be69f3e31fbc69d6f1b9c
Kamil Antropik <kamil.antropik@gmail.com>
2016-07-13 15:11:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var getIP = require('ipware')().get_ip;
+
+app.get('/',function (req,res) {
+  res.send('This is Request Header Parser Microservice API. Use GET request <code>/api/whoami</code> to get simple user information like <ul><li>User IP</li><li>Language</li><li>Software</li></ul>')
+})
+app.get('/api/whoami',function(req, res) {
+  var cryptObj = {};
+  var language = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  var ipInfo = getIP(req);
+  // console.log(ipInfo);
+  // console.log(ip);
+  software = software.match(/\((.*?)\)/)[1];
+  language = language.substr(0,language.indexOf(','));
+
+  cryptObj['ip'] = ipInfo.clientIp;
+  cryptObj['language'] = language;
+  cryptObj['software'] = software;
+  res.json(cryptObj);
+});
+app.listen(port,function() {
+  console.log('Server on port: ', port);
+});
